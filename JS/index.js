b7fce978cd58420643f7c707e754ba61cbe7a8f2
KabiruH <kelkab46@gmail.com>
2022-12-17 19:42:48 +0300

    js started

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,11 @@
+
+//Random Quotes
 fetch ("https://api.gameofthronesquotes.xyz/v1/random")
+//Several random Quotes
+
+//Houses and their members
+fetch ("https://api.gameofthronesquotes.xyz/v1/houses")
+
+
+// Characters list and their quotes
+fetch ("https://api.gameofthronesquotes.xyz/v1/characters")
