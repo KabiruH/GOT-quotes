d52acc2a5896c0ffa1654b0c46101734d2e37cc6
KabiruH <kelkab46@gmail.com>
2022-12-18 00:02:34 +0300

    loading functionality

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,25 @@
+//URLS
+let randomQuotes = "https://api.gameofthronesquotes.xyz/v1/random/5"
+let houses = "https://api.gameofthronesquotes.xyz/v1/houses"
+let charactersList = "https://api.gameofthronesquotes.xyz/v1/characters"
 
-//Random Quotes
-fetch ("https://api.gameofthronesquotes.xyz/v1/random")
+document.addEventListener ("DOMContentLoaded", () => {
 //Several random Quotes
+const loadRandomQuote = () => {
+    fetch (randomQuotes)
+    .then ((res) => res.json())
+    .then ((data => {      
+    }))
+}
+
+
+
 
 //Houses and their members
-fetch ("https://api.gameofthronesquotes.xyz/v1/houses")
+fetch (houses)
 
 
 // Characters list and their quotes
-fetch ("https://api.gameofthronesquotes.xyz/v1/characters")
+fetch (charactersList)
 
+})
