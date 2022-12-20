d679d1538c7e5c1c093cbd7d38c9374443408de5
KabiruH <kelkab46@gmail.com>
2022-12-21 01:43:15 +0300

    new fetch function

--- JS/index.js
+++ JS/index.js
@@ -3,9 +3,12 @@
 document.addEventListener ("DOMContentLoaded", () => {
 
     //Data
-    const getQuotes = document.getElementsByClassName ("quotes")
+    const getQuotes = document.getElementsByClassName ("quotes") 
     const getHouses = document.getElementsByClassName ("houses")
     const getCharacters = document.getElementsByClassName ("Characters")
+    let quotes = []
+    let selectedQuote = null
+    
 
     //Click Events
     const randomQuote = document.getElementById ("getQuote")
@@ -22,17 +25,28 @@ document.addEventListener ("DOMContentLoaded", () => {
 
 function fetchData () {
 fetch ("https://api.gameofthronesquotes.xyz/v1/random/5") 
-    .then (function(response) {
-        return response.json()
+    .then ((response) => response.json())
+    .then((data) =>{
+        quotes = data    
+       //console.log (data)
+
+
+       quotes.forEach((sentence, character) => {
+        const listQuotes = document.createElement("li")
+        listQuotes.textContent = selectedQuote
+
+        listQuotes.addEventListener('click', () => {
+            selectedQuote
+        })
+        getQuotes.appendChild(listQuotes)   
     })
-        .then(function(data) {
-       console.log (data)
+    })
 
 
   
-})
-
 }
+
+
 fetchData ()
 
 
